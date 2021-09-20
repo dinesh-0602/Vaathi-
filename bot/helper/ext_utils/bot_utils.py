a9675cc3555c683fd650d3f60009d94a1807ac49
bodysoda69 <72090512+bodysoda69@users.noreply.github.com>
2021-09-20 21:31:04 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -121,21 +121,19 @@ def get_readable_message():
                 ]:
                     msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
                     if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                        msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                        msg += f"\n<b>Downloaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> / <code>{download.size()}</code>"
                 else:
-                    msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed:</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                    msg += f"\n<b>Uploaded:</b> <code>{get_readable_file_size(download.processed_bytes())}</code> / <code>{download.size()}</code>"
+                msg += f"\n<b>Speed:</b> <code>{download.speed()}</code> \n<b>ETA:</b> <code>{download.eta()}</code> "
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += (
-                        f"\n<b>Seeders:</b> {download.aria_download().num_seeders}"
-                        f" & <b>Peers:</b> {download.aria_download().connections}"
-                    )
+                    msg += f"\n<b>Seeders:</b> <code>{download.aria_download().num_seeders}</code>"
+                    msg += f"\n<b>Peers:</b> <code>{download.aria_download().connections}</code>"
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>To Stop 👉:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-                msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a>'
+                msg += f"\n<b>To Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f'\n<b>User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
             msg += "\n\n"
             if STATUS_LIMIT is not None and INDEX >= COUNT + STATUS_LIMIT:
                 break
